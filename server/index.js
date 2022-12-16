1ad847d21847b630119170ba914a0585bd42acf9
Martin (Office PC) <dev@fortyresources.com>
2022-12-16 17:40:22 +0700

    add server nodejs

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// IMPORT FROM PACKAGES
+const express = require('express');
+const mongoose = require('mongoose');
+
+// IMPORT FROM OTHER FILES
+const authRouter = require('./routes/auth');
+
+// INIT
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://martin:martin123@cluster0.tkctqam.mongodb.net/?retryWrites=true&w=majority"
+
+// middleware
+app.use(express.json);
+app.use(authRouter);
+
+// connection
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log('DB Connection is successful');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+app.get("/", (req, res) => {
+    res.json({msg: "Hello"});
+});
+
+app.listen(PORT, () => {
+    console.log(`connected to port ${PORT}`);
+});
